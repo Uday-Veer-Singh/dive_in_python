02e52cba1a28e6d826cdd25e2c7f0acfed88b636
U-Dev <udaythakur.shely@gmail.com>
2025-12-31 00:24:11 -0800

    initial commit

---
+++ hello.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+print("This is a simple Python script.")
+
+#Data types
+integer_var = 10
+float_var = 20.5
+string_var = "Hello"
+boolean_var = True
+list_var = [1, 2, 3, 4, 5]
+dict_var = {"key1": "value1", "key2": "value2"}
+tuple_var = (1, 2, 3)
+set_var = {1, 2, 3, 4, 5} 
+range_var = range(0, 10)
+none_var = None
+print(integer_var, float_var, string_var, boolean_var, list_var, dict_var, 
+tuple_var, set_var, range_var, none_var)
+
+isinstance(integer_var, float)
+print(isinstance(integer_var, float))
