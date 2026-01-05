2e77126b4b4768a6ff512111a0fd1ba6f7b26029
U-Dev <udaythakur.shely@gmail.com>
2026-01-05 13:18:38 -0800

    arithmetic ops

---
+++ hello.py
@@ -0,0 +1,32 @@
+# Intergers and float numbers
+
+int_num = 4
+float_num = 2.0
+
+add_num = int_num + float_num
+print(add_num)
+
+sub_num = int_num - float_num
+print(sub_num)
+
+multi_num = int_num * float_num
+print(multi_num)
+
+div_num = int_num / float_num
+print(div_num)
+
+# The modulo operator (%) returns the remainder when the value on the left is 
+# divided by the value on the right:
+modulus_num = int_num % float_num
+print(modulus_num)
+
+# Floor division divides two numbers and returns the greatest integer less 
+# than or equal to the result. This is done with the double forward 
+# slash operator (//):
+floor_div_num = int_num // float_num
+print(floor_div_num)
+
+# Exponentiation raises a number to the power of another, and is done with 
+# the double asterisk operator (**):
+exponentiation_num = int_num ** float_num
+print(exponentiation_num)
