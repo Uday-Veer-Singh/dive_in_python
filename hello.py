1a9cf6995a6310c67c2286ea1ae30294b7104d19
U-Dev <udaythakur.shely@gmail.com>
2026-01-05 13:29:53 -0800

    number functions

--- hello.py
+++ hello.py
@@ -30,3 +30,31 @@ print(floor_div_num)
 # the double asterisk operator (**):
 exponentiation_num = int_num ** float_num
 print(exponentiation_num)
+
+# The float() function returns a floating-point number constructed from the 
+# given number:
+float_func = float(int_num)
+print(float_func)
+
+# The int() function returns an integer constructed from the given number:
+int_func = int(float_num)
+print(int_func)
+
+# We can also use these functions on strings
+str_to_int = int('9')
+print(str_to_int)
+str_to_float = float('9.00')
+print(str_to_float)
+
+# round(): Rounds a number to the specified number of decimal places.
+round_num = round(float_num)
+print(round_num)
+
+# abs(): returns the absolute value of a number
+abs_num = abs(-9.8898)
+print(abs_num)
+
+# pow(): raises a number to the power of another or performs modular 
+# exponentiation.
+pow_num = pow(2, 2)
+print(pow_num)
